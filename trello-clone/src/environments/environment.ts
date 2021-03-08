e01982c2cafba982538b11f8bec318c67c92cb8f
leon matias roslin <leonmatias1991@gmail.com>
2021-03-08 18:28:06 -0500

    subiendo cambios

---
+++ trello-clone/src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyC4KFLO7yyHwc7MrvWDxubATAm6W-WwW0A',
+  authDomain: 'trello-clone-143fb.firebaseapp.com',
+  projectId: 'trello-clone-143fb',
+  storageBucket: 'trello-clone-143fb.appspot.com',
+  messagingSenderId: '664728359385',
+  appId: '1:664728359385:web:1780aebe2470f2044109c8',
+  measurementId: 'G-7WVTSG22DK'
+};
+
+export const environment = {
+  production: false,
+  firebaseConfig
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
